a6cbf7d319869c69f2b8438d517f12f435b88096
Dharini <240813.it@rmkec.ac.in>
2026-07-04 14:41:41 +0530

    Reduced embedding memory

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,5 +1,3 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import FakeEmbeddings
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+embedding_model = FakeEmbeddings(size=384)
